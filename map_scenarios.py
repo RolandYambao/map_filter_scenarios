bf810c4a7057a47240fc81ab721d29011d997def
cspdevs000 <cspdevs000@gmail.com>
2022-01-14 16:36:24 -0500

    trying to pull

--- map_scenarios.py
+++ map_scenarios.py
@@ -1,7 +1,17 @@
 # Scenario 1
 
-# Scenario 2
 
+# Scenario 2
+#inflation on products increased to 7%
+prices = [9.99, 12.99, 17.99, 33.99]
+def increase_7_percent(price):
+    return price * 1.07
+
+result = list(map(increase_7_percent, prices))
+print(result)
+
+result2 = list(map(lambda x: x * 1.07, prices))
+print(result2)
 # Scenario 3
 
 # Scenario 4
